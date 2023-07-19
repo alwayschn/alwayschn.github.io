ac22f92d129211e96206ac4466be332c3404fba3
always <like.chn@outlook.com>
2023-07-19 23:37:07 +0800

    问题修改：暗黑模式下页面刷新后切换回白天模式
    1.原因分析：无
    2.修改方法：无

--- themes/Anatolo/source/js/darkLightToggle.js
+++ themes/Anatolo/source/js/darkLightToggle.js
@@ -1,10 +1,47 @@
-function darkLightToggle() {
-    if (document.getElementById("theme-light-mode") != null) {
-        document.getElementById("theme-light-mode").id="theme-dark-mode";
+const getStoredThemeStyle = ()=>localStorage.getItem('theme');
+const setThemeClass = (style)=>{
+    const html = document.documentElement;
+    const prevTheme = [...html.classList].find((c)=>c.match(/theme-(light-mode|dark-mode)/));
+    if (!prevTheme)
         return;
+    console.log(prevTheme)
+    html.classList.remove(prevTheme);
+    html.classList.add(`theme-${style}`);
+
+}
+;
+const setThemeStyle = (style)=>{
+    localStorage.setItem('theme', style);
+    setThemeClass(style);
+}
+;
+const switchTheme = ()=>{
+    const currThemeStyle = getStoredThemeStyle();
+    switch (currThemeStyle) {
+    case 'light-mode':
+        setThemeStyle('dark-mode');
+        break;
+    case 'dark-mode':
+        setThemeStyle('light-mode');
+        break;
+    default:
+        setThemeStyle('light-mode');
+        break;
     }
-    if (document.getElementById("theme-dark-mode") != null) {
-        document.getElementById("theme-dark-mode").id="theme-light-mode";
-        return;
+}
+;
+document.addEventListener('DOMContentLoaded', ()=>{
+    const themeSwitcher = document.querySelector('.themeswitch');
+    themeSwitcher.addEventListener('click', switchTheme, false);
+}
+, false, );
+window.matchMedia('(prefers-color-scheme: dark-mode)').addEventListener('change', switchTheme, false);
+const currThemeStyle = getStoredThemeStyle();
+if (currThemeStyle) {
+    setThemeStyle(currThemeStyle);
+} else {
+    const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark-mode)').matches;
+    if (userPrefersDark) {
+        setThemeStyle('dark-mode');
     }
 }
